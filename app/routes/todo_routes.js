81451bc199c8c6124abbef1fa643ec9c99fc1539
James Bates <james.bates@ge.com>
2018-03-16 14:41:15 -0500

    Get all todos is working. Now why isn't anything rendering

--- app/routes/todo_routes.js
+++ app/routes/todo_routes.js
@@ -1,6 +1,9 @@
 //todo routes
 module.exports = function (app, db) {
+
+
   app.post('/api/todos', function (req, res) {
+    // Create TODO object from request.
     const todo = {
       _id: req.body._id,
       title: req.body.title,
@@ -8,7 +11,8 @@ module.exports = function (app, db) {
       date: req.body.date,
       status: req.body.status
     };
-    console.log('LOGGING VALUES', db);
+
+    // Add the TODO to the database.
     db.collection('todo').insertOne(todo)
       .then(function (result) {
         res.send(result.ops[0]);
@@ -18,23 +22,16 @@ module.exports = function (app, db) {
       })
   });
 
-  // app.get('/api/todos', function (req, res) {
-  //   const todo = {
-  //     _id: req.body._id,
-  //     title: req.body.title,
-  //     description: req.body.description,
-  //     date: req.body.date,
-  //     status: req.body.status
-  //   };
-  //
-  //   db.collection('todos').insertOne(todo)
-  //     .then(function (result) {
-  //       res.send(result.ops[0]);
-  //     })
-  //     .catch(function (error) {
-  //       res.send({ 'error': 'An error occured while creating the todo: ' + error.message });
-  //     })
-  // });
+  app.get('/api/todos', function (req, res) {
+    db.collection('todo').find().toArray()
+      .then(function (result) {
+        res.send(result);
+      })
+
+      .catch(function (error) {
+        res.send({'error': 'An error occured pulling your todos: ' + error.message});
+      })
+  });
 
 
 };
